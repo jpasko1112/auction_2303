de5200152f2296f34926940786bd6c0cf58554f9
Justin Pasko <jpasko1112@gmail.com>
2023-04-26 12:58:13 -0400

    Add method and test for bidders.

--- lib/auction.rb
+++ lib/auction.rb
@@ -20,4 +20,10 @@ class Auction
   def potential_revenue
     @items.sum { |item| item.current_high_bid.to_i }
   end
+
+  def bidders
+    # @attendees.select { |attendee| attendee.has_bids? }.map { |attendee| attendee.name }
+    @items.flat_map { |item| item.bids.keys.map { |bidder| bidder.name } }.uniq
+    # require 'pry'; binding.pry
+  end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -57,6 +57,9 @@ RSpec.describe Auction do
 
   describe '#potential_revenue' do
     it 'calculates the potential revenue' do
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
       @auction.add_item(@item1)
       @auction.add_item(@item2)
       @auction.add_item(@item3)
@@ -67,6 +70,22 @@ RSpec.describe Auction do
       @item4.add_bid(@attendee3, 50)
       @item3.add_bid(@attendee2, 15)
       expect(@auction.potential_revenue).to eq(87)
+    end
+  end
+
+  describe '#bidders' do
+    it 'returns an array of strings of bidders names' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+
+      expect(@auction.bidders).to eq(['Megan', 'Bob', 'Mike'])
     end
   end
 end
